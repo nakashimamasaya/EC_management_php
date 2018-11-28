c533286d539c74a31438a2e43325925983b86ace
Masaya Nakashima <kawa18sima@gmail.com>
2018-11-28 16:12:35 +0900

    Remove features not related to the product management function
    Remove features not related to the product management function

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -15,34 +15,6 @@ class ProductsController extends AppController
         $this->Auth->allow([]);
     }
 
-    public function index()
-    {
-
-        $this->paginate = [
-            'limit' => 10,
-            'contain' => ['Users'],
-            'order' => [
-                'saleDate' => 'desc'
-            ]
-        ];
-        $products = $this->paginate($this->Products);
-        $this->set(compact('products'));
-        $this->set('today', date("Y/m/d"));
-    }
-
-    public function index2(){
-        $this->paginate = [
-            'limit' => 12,
-            'contain' => ['Users'],
-            'order' => [
-                'saleDate' => 'desc'
-            ]
-        ];
-        $products = $this->paginate($this->Products);
-        $this->set(compact('products'));
-        $this->set('today', date("Y/m/d"));
-    }
-
     public function managements(){
         $current_user = $this->Auth->user();
         if($this->UserLevel->checkUser($current_user)){
@@ -64,15 +36,6 @@ class ProductsController extends AppController
     {
         $product = $this->Products->get($id, [
             'contain' => ['CategoriesProduct' => ['Categories']]
-        ]);
-
-        $this->set('product', $product);
-    }
-
-    public function details($id = null){
-        $product = $this->Products->get($id, [
-            'contain' => ['CategoriesProduct' => ['Categories'],
-                        'Users']
         ]);
 
         $this->set('product', $product);
@@ -211,31 +174,4 @@ class ProductsController extends AppController
 
         return $this->redirect(['action' => 'managements']);
     }
-
-    public function find($find = null){
-        if($this->request->is('post')){
-            $find = $this->request->getData('find');
-            $find = urlencode($find);
-            $this->redirect(['action' => 'find', $find]);
-        }else if(isset($find)){
-            $this->paginate = [
-            'limit' => 10,
-            'contain' => ['Users'],
-            'order' => [
-                'saleDate' => 'desc'
-            ]];
-            $results = $this->Products->find('all')->where(["OR" => [["title like " => "%" . $find . "%"],["details like " => "%" . $find . "%"]]]);
-            $results = $this->paginate($results);
-            $find = urldecode($find);
-            $flag = true;
-            $this->set(compact('find', 'results', 'flag'));
-            $this->set('today', date("Y/m/d"));
-        }else{
-            $results = '';
-            $flag = false;
-            $this->set(compact('find', 'results', 'flag'));
-        }
-
-    }
-
 }
